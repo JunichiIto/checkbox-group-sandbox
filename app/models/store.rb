599bc03425a94567c530ea274e4c199df1171fa4
Junichi Ito <jit@sonicgarden.jp>
2015-03-24 21:41:05 +0900

    Remove blank value.

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,4 +1,9 @@
 class Store < ActiveRecord::Base
   FACILITY_OPTIONS = {'1' => :parking, '2' => :private_room, '3' => :card_ok}
   serialize :facilities, Array
+
+  before_save :remove_blank_facility_option
+  def remove_blank_facility_option
+    self.facilities = self.facilities.select(&:present?)
+  end
 end
