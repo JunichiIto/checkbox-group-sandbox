6432012af8171af43254961012baee382a99f5ad
Junichi Ito <jit@sonicgarden.jp>
2015-03-25 04:52:19 +0900

    Refactoring.

--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,12 @@
 class Store < ActiveRecord::Base
-  FACILITY_OPTIONS = {'1' => '駐車場あり', '2' => '個室あり', '3' => 'クレジットカード可'}
+  FACILITY_OPTIONS = { '1' => '駐車場あり', '2' => '個室あり', '3' => 'クレジットカード可' }
   serialize :facilities, Array
+
+  def selected_facility_options_text
+    selected_facility_options.join(', ')
+  end
+
+  private
 
   before_save :reject_empty_facility_options!
   def reject_empty_facility_options!
@@ -10,9 +16,5 @@ class Store < ActiveRecord::Base
 
   def selected_facility_options
     FACILITY_OPTIONS.slice(*facilities).values
-  end
-
-  def selected_facility_options_text
-    selected_facility_options.join(', ')
   end
 end
