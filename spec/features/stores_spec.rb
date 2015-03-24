8da98c8179dcc663f8ae3e1306e36f3903f678c8
Junichi Ito <jit@sonicgarden.jp>
2015-03-25 04:47:04 +0900

    Add expectations.

--- spec/features/stores_spec.rb
+++ spec/features/stores_spec.rb
@@ -10,5 +10,18 @@ feature 'Stores', type: :feature do
     click_on 'Create Store'
     expect(page).to have_content 'Store was successfully created.'
     expect(page).to have_content '駐車場あり, クレジットカード可'
+
+    click_on 'Edit'
+    expect(page).to have_checked_field('駐車場あり')
+    expect(page).to have_checked_field('クレジットカード可')
+    expect(page).to have_unchecked_field('個室あり')
+    uncheck '駐車場あり'
+    uncheck 'クレジットカード可'
+    click_on 'Update Store'
+    expect(page).to have_content 'Store was successfully updated.'
+    expect(page).to_not have_content '駐車場あり'
+    expect(page).to_not have_content '個室あり'
+    expect(page).to_not have_content 'クレジットカード可'
+    expect(Store.last.facilities.size).to eq 0
   end
 end
